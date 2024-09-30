4896a5d7d86a2b2193150fb7adda925c2900c926
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-01 03:50:58 +0530

    add data types; custom data types;

---
+++ 2 basics/src/custom-types.ts
@@ -0,0 +1,37 @@
+interface Contact extends Address {
+    id: number;
+    name: string;
+    birthDate?: Date; // making optional by using ?
+}
+
+// Type alias
+// Custom aliases can be given to data types
+type AddressPC = string
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: AddressPC; // usage of custom data alias
+}
+
+let primaryContact: Contact = {
+    // birthDate: new Date("01-01-1980"),
+    id: 12345,
+    name: "Jamie Johnson",
+    postalCode: "21",
+    line1: "s",
+    line2: "d",
+    province: "p",
+    region: "m",
+}
+
+let myAddres: Address = {
+    line1: "s",
+    line2: "w",
+    province: "south",
+    region: "SL",
+    postalCode: "81K"
+}
+
---
+++ 2 basics/src/data-types.ts
@@ -0,0 +1,12 @@
+let x: number
+let y: string
+let z: boolean
+let a: Date
+let b: string[]
+let k: any
+
+b = "Hello!" as any
+b = 1234 as any
+
+k = "Hello!" as any
+k = 1234
